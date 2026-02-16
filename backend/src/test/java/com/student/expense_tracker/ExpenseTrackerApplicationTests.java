2f167f4a9e6ecb71e622905fd44c41f73e7b1c7a
Jagadeesh <jagachellapu2005@gmail.com>
2026-02-16 14:33:27 +0530

    Initial Project structure

---
+++ backend/src/test/java/com/student/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
